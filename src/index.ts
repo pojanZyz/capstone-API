d0c3885b6a18ba6805f59e75247d1152f7283ffe
pojanZyz <akunword0@gmail.com>
2025-03-23 17:15:03 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Request, Response, NextFunction } from "express";
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,11 +9,21 @@ const adminRouter = require("./router/admin");
 const dotenv = require("dotenv");                            
 
 dotenv.config();
-app.use(cors({
-    origin: "*", // Mengizinkan semua origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Metode yang diizinkan
-    allowedHeaders: ["Content-Type"] // Header yang diizinkan
-}));
+app.use(cors());
+
+app.options('*', cors()); // Menangani preflight request
+
+app.use((req : Request, res : Response, next : NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
+
 app.use(express.json());
 
 app.use('/users', usersRouter);
