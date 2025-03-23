4e8e852e3294778558e9c9c67b174cd25f823e94
pojanZyz <akunword0@gmail.com>
2025-03-23 17:50:54 +0700

    plisssssssssss

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 app.use(cors({
-    origin: "http://127.0.0.1:5500", // Ganti dengan domain frontend-mu
+    origin: ["http://127.0.0.1:5500"], // Ganti dengan domain frontend-mu
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true, // Jika frontend butuh mengirim cookie/token
@@ -20,6 +20,15 @@ app.use(cors({
 app.options("/", cors());
 
 app.use(express.json());
+
+app.use((req : Request, res : Response, next : NextFunction) => {
+    if (req.method === "OPTIONS") {
+        res.status(200).send("OK"); // Pastikan statusnya 200
+    } else {
+        next();
+    }
+});
+
 
 app.use('/users', usersRouter);
 app.use('/', adminRouter);
