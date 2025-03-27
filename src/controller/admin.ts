7a414a79fa704c2251b526eabd899444f53624bd
pojanZyz <akunword0@gmail.com>
2025-03-27 23:17:26 +0700

    fix

--- src/controller/admin.ts
+++ src/controller/admin.ts
@@ -13,9 +13,10 @@ const createNewUser = async (req : express.Request, res : express.Response) => {
         const hashedPass = await bcrypt.hash(password, 10);
         const result = await prisma.users.create({ data: { username, password : hashedPass, email, role: "user" } });
         res.json({ message: "CREATE USER SUCCESS", data: result });
-        await prisma.$disconnect();
     } catch (error) {
         res.json({ message: "CREATE USER UNSUCCESS", error });
+    }finally{
+        await prisma.$disconnect();
     }
 };
 
@@ -23,9 +24,10 @@ const getAllUsers =  async (req : express.Request, res : express.Response) => {
     try {
         const result = await prisma.users.findMany();
         res.json({ message: "GET ALL USERS SUCCESS", data: result });
-        await prisma.$disconnect();
     } catch (error) {
         res.json({ message: "GET ALL USER UNSUCCESS", error });
+    }finally{
+        await prisma.$disconnect();
     }
 };
 
@@ -45,9 +47,10 @@ const updateUser =  async (req : express.Request, res : express.Response) => {
             data: updateData
         });
         res.json({ message: "UPDATE USER SUCCESS", data: result });
-        await prisma.$disconnect();
     } catch (error) {
         res.json({ message: "UPDATE USER UNSUCCESS", error });
+    }finally{
+        await prisma.$disconnect();
     }
 };
 
@@ -77,6 +80,8 @@ const deleteUser = async (req: express.Request, res: express.Response) => {
     } catch (error) {
         console.error("Error deleting user:", error);
         res.status(500).json({ message: "DELETE USER UNSUCCESS", error });
+    }finally{
+        await prisma.$disconnect();
     }
 };
 
--- src/controller/users.ts
+++ src/controller/users.ts
@@ -43,10 +43,11 @@ const register = async (req : express.Request, res : express.Response) => {
         });
 
         res.json({ message: "REGISTER IS SUCCESS", data: result });
-        await prisma.$disconnect();
     } catch (error) {
         console.error("Register error:", error);
         res.status(500).json({ message: "REGISTER IS UNSUCCESS", error: error });
+    }finally{
+        await prisma.$disconnect();
     }
 };
 
@@ -72,10 +73,11 @@ const login = async (req: express.Request, res: express.Response) => {
             data: payload,
             token
         });
-        await prisma.$disconnect();
     } catch (error) {
         console.error("Login error:", error);
         res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+    } finally {
+        await prisma.$disconnect(); // Tutup koneksi Prisma setelah query selesai
     }
 };
 
@@ -88,10 +90,11 @@ const logout = async (req: ValidationRequest, res: express.Response) => {
     }
 
     res.json({ message: "Logout berhasil" });
-    await prisma.$disconnect();
     }catch (error) {
         console.error("logout error:", error);
         res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+    } finally {
+        await prisma.$disconnect(); // Tutup koneksi Prisma setelah query selesai
     }
 };
 
@@ -105,9 +108,10 @@ const getUserLogin =  async (req: ValidationRequest, res: express.Response) => {
         if (!user) return res.status(404).json({ message: "User not found" });
 
         res.json({ message: "GET USER SUCCESS", data: user });
-        await prisma.$disconnect();
     } catch (error) {
         res.status(500).json({ message: "ERROR GETTING USER", error });
+    }finally{
+        await prisma.$disconnect();
     }
 };
 
