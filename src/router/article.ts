fd066defc1136e8488b82ef238dc2284beec0cb7
pojanZyz <akunword0@gmail.com>
2025-04-08 01:50:35 +0700

    add access validation

--- src/router/article.ts
+++ src/router/article.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 const { createArticle, getAllArticles, updateArticle, deleteArticle } = require('../controller/articleControl');
+const { accessValidation } = require('../middleware/accessValidation'); // Import middleware accessValidation
+const { adminValidation } = require('../middleware/adminValidation'); // Import middleware adminValidation
 const upload = require('../middleware/upload'); // Import middleware multer
 const router = express.Router();
 
-router.post('/articles', upload.single('image'), createArticle);
-router.get('/articles', getAllArticles);
-router.patch('/articles/:id', upload.single('image'), updateArticle);
-router.delete('/articles/:id', deleteArticle);
+router.post('/articles', upload.single('image'), accessValidation, adminValidation, createArticle);
+router.get('/articles', accessValidation, adminValidation, getAllArticles);
+router.patch('/articles/:id', upload.single('image'), accessValidation, adminValidation, updateArticle);
+router.delete('/articles/:id', accessValidation, adminValidation, deleteArticle);
 
 module.exports = router;
