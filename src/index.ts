f66a76afbc2924e4d776a5075785f3d3275334ee
pojanZyz <akunword0@gmail.com>
2025-04-13 17:44:12 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,7 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: '*', // Atau ganti dengan origin spesifik seperti 'http://127.0.0.1:5500'
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-app.options('*', cors()); // Tangani preflight request
+app.use(cors());
 app.use(express.json());
 
 app.use('/users', usersRouter);
