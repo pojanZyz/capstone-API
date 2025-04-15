816431f7b970b75590cc74117f848bcb86c6a440
pojanZyz <akunword0@gmail.com>
2025-04-16 01:00:08 +0700

    fix

--- src/controller/mlControl.ts
+++ src/controller/mlControl.ts
@@ -3,7 +3,8 @@ import * as tf from '@tensorflow/tfjs-node';
 
 // kasih tipe tf.LayersModel | null
 let model: tf.LayersModel | null = null;
-const modelUrl = 'https://ggwfplbytoyuzuevhcfo.supabase.co/storage/v1/object/public/model//model_wisata.json';
+const modelUrl = 'https://ggwfplbytoyuzuevhcfo.supabase.co/storage/v1/object/public/model/model_wisata.json';
+
 const loadModel = async (): Promise<void> => {
   if (!model) {
     model = await tf.loadLayersModel(modelUrl);
@@ -16,7 +17,8 @@ const predictFromInput = async (input: number[]): Promise<number[]> => {
   const tensorInput = tf.tensor2d([input], [1, input.length]);
   const prediction = model!.predict(tensorInput) as tf.Tensor;
   const result = await prediction.data();
-
+  tensorInput.dispose();
+  prediction.dispose();
   return Array.from(result);
 };
 
