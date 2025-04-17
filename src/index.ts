8d457c229c138cdefa65d56b2d00727573a03eb3
pojanZyz <akunword0@gmail.com>
2025-04-18 01:04:20 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const mlRouter = require("./router/ml");
 dotenv.config();
 const app = express();
 
-app.use(cors({origin: "http://127.0.0.1:5500/"}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/users', usersRouter);
